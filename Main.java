84b1cc4ee35a65538049efe73c5e441471223e85
Aaron Hirtle <aaronhirtle85@gmail.com>
2025-01-18 14:42:58 -0400

    Add RPGCharacter class for encapsulation example

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+/***********************************************************************
+ * Author: Aaron Hirtle                                                  
+ * Date:   2025-01-18                                               
+ * Class:  Intro to Object Oriented Programming PROG1400                                                 
+ * Assignment: Exercise 2                           
+ ***********************************************************************/
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Create new RPG objects
+        RPGCharacter character1 = new RPGCharacter("Cloud", "Hero", 90, 25);
+        RPGCharacter character2 = new RPGCharacter("Sephiroth", "Villain", 500, 60);
+
+        // Setters
+        character1.setHealth(90);
+        character1.setStrength(25);
+
+        // Getters
+        System.out.println("**Character One**");
+        System.out.println("Name: " + character1.getName());
+        System.out.println("Role: " + character1.getRole());
+        System.out.println("Health: " + character1.getHealth());
+        System.out.println("Strength: " + character1.getStrength());
+
+        System.out.println("\n**Character Two**");
+        System.out.println("Name: " + character2.getName());
+        System.out.println("Role: " + character2.getRole());
+        System.out.println("Health: " + character2.getHealth());
+        System.out.println("Strength: " + character2.getStrength());
     }
 }
---
+++ RPGCharacter.java
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Author: Aaron Hirtle                                                  
+ * Date:   2025-01-18                                               
+ * Class:  Intro to Object Oriented Programming PROG1400                                                 
+ * Assignment: Exercise 2                           
+ ***********************************************************************/
+
+
+
+public class RPGCharacter {
+    private String name;
+    private String role;  
+    private int health;
+    private int strength;
+
+    // Initialize
+    public RPGCharacter(String name, String role, int health, int strength) {
+        this.name = name;
+        this.role = role;
+        this.health = health;
+        this.strength = strength;
+    }
+
+    // Getter and Setter Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+}
